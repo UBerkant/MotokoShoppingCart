5c32b1048d9e81001f54908dec6d1b382b3cd4cc
Uğur Berkant Ediz <108654354+UBerkant@users.noreply.github.com>
2024-10-27 22:54:39 +0300

    Update and rename Header.js to Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hoş Geldiniz!</h1>
+      <p>Ürünlerinizi eklemek için "Ürün Ekle" sayfasına gidin.</p>
+    </div>
+  );
+};
+
+export default Home;
